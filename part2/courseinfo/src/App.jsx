78d38be7b55662514dce35f409796c575b77dd9b
alifzwan <alifzakwan529@gmail.com>
2024-04-19 16:47:06 +0800

    Add course information steps 6-10 and separate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import Course from './component/Course'
+
+//2.1: Course information step6
+//2.2: Course information step7
+//2.3: Course information step8
+//2.4: Course information step9
+//2.5: Separate module step10
+
 const App = () => {
   return (
    <div>
