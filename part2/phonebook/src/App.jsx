38741385cd3615fe98ba720b12d710c203e91be6
alifzwan <alifzakwan529@gmail.com>
2024-04-25 11:09:45 +0800

    Add error handling for updating existing person in App.jsx

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -17,6 +17,8 @@ import Notification from './components/Notification'
 // 2.13: The Phonebook step 8
 // 2.14: The Phonebook step 9
 // 2.15*: The Phonebook step 10
+// 2.16: Phonebook step 11
+
 
 const App = () => {
   
@@ -24,13 +26,22 @@ const App = () => {
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [searchName, setSearchName] = useState('')
-    const [notification, setNotification] = useState('')
+    const [notification, setNotification] = useState(null)
+    const [error, setError] = useState(null)
+    
 
 
     const showMessage = (message) => {
       setNotification(message)
       setTimeout(() => {
         setNotification(null)
+      }, 5000)
+    }
+
+    const showError = (error) => {
+      setError(error)
+      setTimeout(() => {
+        setError(null)
       }, 5000)
     }
     
@@ -50,8 +61,6 @@ const App = () => {
     const addPerson = (event) => {
       event.preventDefault()
       const personExists = persons.find(person => person.name === newName ) // Check if name already exists in phonebook
-      
-
       if (personExists) { 
           const result = window.confirm(`${newNumber} is already added to phonebook, replace the old number with a new one?`)
             if (result){
@@ -59,8 +68,12 @@ const App = () => {
               personsService
                 .update(personExists.id, changedPerson)
                 .then(returnedPerson => {
-                  setPersons(persons.map(person => person.id !== personExists.id ? person : returnedPerson))
+                  setPersons(persons.map(person => person.id !== personExists.id ? person : returnedPerson)) // Update the number
                   showMessage(`Updated ${newName}`)
+                })
+                .catch(error => {
+                  showError(`Information of ${newName} has already been removed from the server`)
+                  setPersons(persons.filter(person => person.id !== personExists.id)) // Remove the person from the list
                 })
             }
       } else {
@@ -113,7 +126,7 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <Notification message={notification}/>
+            <Notification message={notification} error={error} />
             <Filter value={searchName} onChange={handleSearchChange}/>
 
 
