cf2a2608dcd5aa1b90ec99e865f4b8f250103f56
alifzwan <alifzakwan529@gmail.com>
2024-05-08 11:06:10 +0800

    chore: Update App component to include Filter and Country components, and implement country search functionality

--- part2/dataForCountries/src/App.jsx
+++ part2/dataForCountries/src/App.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import countriesService from './services/countries'
+import Filter from './components/Filter'
+import Country from './components/Country'
+
+// 2.18* Data for countries, step 1
+// 2.19*: Data for countries, step 2
+
 
 const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchCountry, setSearchCountry] = useState('')
+
+
+  useEffect(() => {
+    countriesService.getAll()
+      .then(data => {
+        setCountries(data)
+        console.log(data)
+      })
+  }, [])
+  
+  const handleSearchChange = (event) => {
+    setSearchCountry(event.target.value)
+  }
+
+  const filteredCountries = Array.isArray(countries) ? countries.filter(country => country.name.common.toLowerCase().includes(searchCountry.toLowerCase())) : []
+
+
   return (
-    <div>App</div>
+    <div>
+      <Filter value={searchCountry} onChange={handleSearchChange}/>
+      {
+        // if there are more than 10 countries that match the search, the user is asked to make the search more specific
+        filteredCountries.length > 10 ? 
+            <p>Too many matches, please make your query more specific.</p> 
+        : 
+        // if there are 2-10 countries that match the search, the names of the countries are displayed
+        filteredCountries.length > 1 ? 
+            filteredCountries.map((country) => ( //
+              <p key={country.cca3}>{country.name.common}</p> 
+            ))
+        :
+        // if there is only one country that matches the search, detailed information about the country is displayed
+        filteredCountries.length === 1 ? 
+            <Country countries={filteredCountries} />
+        : 
+        null
+      }
+    </div>
   )
 }
 
