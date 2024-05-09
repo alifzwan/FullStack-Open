82d75c903e3f6ff29e0959f85999b15c8825717c
alifzwan <alifzakwan529@gmail.com>
2024-05-09 12:20:12 +0800

    feat: Add script to insert new blog into MongoDB
    This commit adds a new script `mongo.js` that allows inserting a new blog into the MongoDB database. The script takes the password, title, author, url, and likes as command-line arguments and creates a new `Blog` document with the provided values. It then saves the document to the database and prints all existing blogs.
    
    This change is necessary to provide a convenient way to add new blogs to the database for testing and development purposes.

---
+++ part4/BlogList/mongo.js
@@ -0,0 +1,55 @@
+const mongoose = require ('mongoose')
+
+if(process.argv.length < 3){
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const title = process.argv[3]
+const author = process.argv[4]
+const url = process.argv[5]
+const likes = process.argv[6]
+
+const MongoUrl = 
+`mongodb+srv://Blog:${password}@cluster0.0ovcfev.mongodb.net/?
+retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(MongoUrl)
+
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const blog = new Blog({
+    title: title,
+    author: author,
+    url: url,
+    likes: likes
+})
+
+Blog.find({}).then(result => {
+    if(result.length > 0){
+        result.forEach(blog => {
+            console.log(blog)
+        })
+    } else {
+        console.log('No blogs found')
+    }
+    mongoose.connection.close()
+})
+
+// blog.save().then(result => {
+//     console.log(`Blog saved: ${result}`)
+//     mongoose.connection.close()
+// })
+
+
