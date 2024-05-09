4ff68f27289f176926b50f7329026c848ad94640
alifzwan <alifzakwan529@gmail.com>
2024-05-09 12:19:48 +0800

    chore: Add ESLint configuration file

---
+++ part4/BlogList/eslint.config.mjs
@@ -0,0 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  pluginJs.configs.recommended,
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  
+];
