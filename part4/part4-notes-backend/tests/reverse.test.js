56fc594f9bbd534295df058e9019f90796127369
alifzwan <alifzakwan529@gmail.com>
2024-05-10 15:57:11 +0800

    chore: Add unit tests for reverse function

--- part4/part4-notes-backend/tests/reverse.test.js
+++ part4/part4-notes-backend/tests/reverse.test.js
@@ -1,14 +1,27 @@
-const { test } = require('node:test')
-const assert = require('node:assert')
+//! Unit Testing
+// - These are used to test individual units of code, such as functions
+// - They are typically small, quick to write, and run quickly
 
-const reverse = require('../utils/for_testing').reverse // import the reverse function
 
+const { test } = require('node:test') // import the test function for defining tests
+const assert = require('node:assert') // import the assert function for assertion
 
+const reverse = require('../utils/for_testing').reverse 
+
+// - It'll check the reverse function when the input is 'a'
+// - Since the reverse of a single character is the same character, the expected result is 'a'
+// - assert.strictEqual() used to check that the actual output of the reverse function is equal to the expected output
 test('reverse of a', () => {
     const result = reverse('a')
 
     assert.strictEqual(result, 'a')
 })
+
+
+
+// - It'll check the reverse function when the input is 'react'
+// - Since the reverse of 'react' is 'tcaer', the expected result is 'tcaer'
+// - assert.strictEqual() used to check that the actual output of the reverse function is equal to the expected output
 
 test('reverse of react', () => {
     const result = reverse('react')
@@ -16,6 +29,9 @@ test('reverse of react', () => {
     assert.strictEqual(result, 'tcaer')
 })
 
+// - It'll check the reverse function when the input is 'saippuakauppias'
+// - Since the reverse of 'saippuakauppias' is 'saippuakauppias', the expected result is 'saippuakauppias'
+// - assert.strictEqual() used to check that the actual output of the reverse function is equal to the expected output
 test('reverse of saippuakauppias', () => {
     const result = reverse('saippuakauppias')
   
