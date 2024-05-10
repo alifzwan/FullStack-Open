e28cf6aa32710d284d6ad69f7474318aadcda5ae
alifzwan <alifzakwan529@gmail.com>
2024-05-10 11:03:03 +0800

    chore: Update BlogList server configuration and middleware

--- part4/BlogList/index.js
+++ part4/BlogList/index.js
@@ -3,6 +3,10 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const app = require('./app')
 
+// 4.1 Blog List, step 1
+// 4.2 Blog List, step 2
+
+
 
 app.listen(config.port, () => {
     logger.info(`Server running on port ${config.port}`)
