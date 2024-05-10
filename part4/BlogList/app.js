0f368d9588cb2cb2c450739e562fe3732b4f3ac2
alifzwan <alifzakwan529@gmail.com>
2024-05-10 10:27:30 +0800

    chore: Update app.js to include MongoDB connection, middleware, and router
    This commit adds the necessary code to `app.js` to establish a connection to MongoDB, set up middleware for request logging and error handling, and include the router for handling blog-related routes. It also includes the necessary dependencies and configurations for CORS, serving static files, and parsing JSON payloads.
    
    These changes are necessary to ensure proper functioning of the application and to provide a structured and organized codebase.

---
+++ part4/BlogList/app.js
@@ -0,0 +1,36 @@
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blogs')
+
+
+const cors = require('cors') 
+const express = require('express')
+const app = express()
+
+
+const mongoose = require ('mongoose')
+mongoose.set('strictQuery', false)
+
+
+logger.info('Connecting to', config.mongoUrl)
+mongoose.connect(config.mongoUrl)
+    .then(result => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch(error => {
+        logger.error('Error connecting to MongoDB:', error.message)
+    })
+
+
+app.use(cors()) // The cors middleware is used to allow requests from all origins.
+app.use(express.static('dist')) // The express.static middleware is used to serve static files from the dist directory.
+app.use(express.json()) // The express.json middleware is used to parse JSON payloads in the request body.
+    
+app.use(middleware.requestLogger) // The requestLogger middleware is used to log information about the requests that are made to the server.
+app.use('/api/blogs', blogRouter)
+app.use(middleware.unknownEndpoint) // The unknownEndpoint middleware is used to catch requests made to unknown routes.
+app.use(middleware.errorHandler) // The errorHandler middleware is used to catch errors in request handling.
+
+
+module.exports = app
