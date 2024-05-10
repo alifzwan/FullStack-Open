69d20fdb7176f38f2763dfb74daad60127797758
alifzwan <alifzakwan529@gmail.com>
2024-05-10 10:08:09 +0800

    chore: Update base URL for API requests to '/api/blogs'

--- part4/BlogList/client/src/services/blogs.js
+++ part4/BlogList/client/src/services/blogs.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/blogs'
+const baseUrl = 'http://localhost:3001/api/blogs'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
