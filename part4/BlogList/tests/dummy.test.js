2d63eb15d95d5838827caa0a8e6e4b5df90f6c49
alifzwan <alifzakwan529@gmail.com>
2024-05-13 11:03:10 +0800

    Add unit tests for author with most blog and author with most likes

--- part4/BlogList/tests/dummy.test.js
+++ part4/BlogList/tests/dummy.test.js
@@ -130,3 +130,88 @@ describe('favourite blog', () => {
         })
     })
 })
+
+describe('Author with most blog', () => {
+    const blogs = [
+      {
+        title: "React patterns",
+        author: "Michael Chan",
+        likes: 7,
+      },
+      {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        likes: 5,
+      },
+      {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12,
+      },
+      {
+        title: "First class tests",
+        author: "Robert C. Martin",
+        likes: 10,
+      },
+      {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        likes: 0,
+      },
+      {
+        title: "Type wars",
+        author: "Robert C. Martin",
+        likes: 2,
+      }  
+    ]
+    test('author with most blog', () => {
+        const result = listHelper.mostBlogs(blogs)
+        assert.deepStrictEqual(result, {
+          author: "Robert C. Martin",
+          blogs: 3
+        })
+    })
+})
+
+describe('Author with most likes', () => {
+  const blogs = [
+    {
+      title: "React patterns",
+      author: "Michael Chan",
+      likes: 7,
+    },
+    {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    },
+    {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    },
+    {
+      title: "First class tests",
+      author: "Robert C. Martin",
+      likes: 10,
+    },
+    {
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      likes: 0,
+    },
+    {
+      title: "Type wars",
+      author: "Robert C. Martin",
+      likes: 2,
+    }  
+  ]
+
+  test('author with most likes', () => {
+      const result = listHelper.mostLikes(blogs)
+      assert.deepStrictEqual(result, {
+        author: "Edsger W. Dijkstra",
+        likes: 17
+      })
+  })
+})
