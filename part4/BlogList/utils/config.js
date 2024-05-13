232002687c067805e563bed71ade66c729f0f2f7
alifzwan <alifzakwan529@gmail.com>
2024-05-13 11:03:23 +0800

    refactor: Update config.js to use separate mongoUrl for test environment

--- part4/BlogList/utils/config.js
+++ part4/BlogList/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
-const mongoUrl = process.env.mongoUrl
 const port = process.env.port
+const mongoUrl = process.env.NODE_ENV === 'test'
+    ? process.env.test_mongoUrl
+    : process.env.mongoUrl
 
 module.exports = {
     mongoUrl,
