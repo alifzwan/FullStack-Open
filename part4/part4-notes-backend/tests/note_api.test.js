a497d62055ccee4c915749d7ccf4706fb1eff7b3
alifzwan <alifzakwan529@gmail.com>
2024-05-13 14:17:59 +0800

    feat: Add unit tests for note_api endpoints

---
+++ part4/part4-notes-backend/tests/note_api.test.js
@@ -0,0 +1,60 @@
+const { test, after, beforeEach} = require('node:test') //import test and after functions from node:test
+const Note = require('../models/note') //import the Note model
+const assert = require('node:assert') // import the assert function for assertion
+const mongoose = require('mongoose') //import mongoose
+const supertest = require('supertest') //import supertest
+const app = require('../app') //import app
+
+// create an instance of supertest that is tied to the app
+// This object is used to make HTTP requests to the app
+const api = supertest(app) 
+
+const initialNotes =[
+    {
+        content: 'HTML is easy',
+        important: false,
+    },
+    {
+        content: 'Browser can execute only JavaScript',
+        important: true,
+    },
+]
+
+beforeEach(async () => {
+    await Note.deleteMany([])
+    let noteObject = new Note(initialNotes[0])
+    await noteObject.save()
+    noteObject = new Note(initialNotes[1])
+    await noteObject.save()
+})
+
+
+
+
+
+
+
+test('notes are returned as json', async () => {
+    console.log(initialNotes)
+    await api
+        .get('/api/notes') //make a get request to /api/notes
+        .expect(200) //expect status code 200
+        .expect('Content-Type', /application\/json/) //expect content type to be application/json
+})
+
+test('there are two notes', async () => {
+    const response = await api.get('/api/notes') //make a get request to /api/notes
+    assert.strictEqual(response.body.length, initialNotes.length) //expect the length of the response body to be 2
+})
+
+test('the first note is about HTTP methods', async () => {
+    const response = await api.get('/api/notes') //make a get request to /api/notes
+
+    const contents = response.body.map(e => e.content) //get the content of all notes
+    assert.strictEqual(contents.includes('HTML is easy', true))
+})
+
+
+after(async () => { //close the connection to the database after all tests have been run
+    await mongoose.connection.close()
+})
