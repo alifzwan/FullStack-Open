d60424fda849fa7d807130fd1312929f3d75982d
alifzwan <alifzakwan529@gmail.com>
2024-05-15 09:37:27 +0800

    chore: Add requestLogger middleware for logging request details

--- part4/part4-notes-backend/utils/middleware.js
+++ part4/part4-notes-backend/utils/middleware.js
@@ -3,6 +3,7 @@ const logger = require('./logger')
 
 // requestLogger middleware
 const requestLogger = (request, response, next) => {
+    logger.info('---')
     logger.info('Method:', request.method)
     logger.info('Path:  ', request.path)
     logger.info('Body:  ', request.body)
