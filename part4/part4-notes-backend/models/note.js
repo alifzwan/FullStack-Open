4626f433c67bf8b37cc4dd73d05a049ccd4d3ed7
alifzwan <alifzakwan529@gmail.com>
2024-05-16 10:36:23 +0800

    chore: Update note schema in note.js model

--- part4/part4-notes-backend/models/note.js
+++ part4/part4-notes-backend/models/note.js
@@ -1,4 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // import mongoose
+
+/* Define the schema for the Note
+
+    Schema - structure that define the shape of the document
+             within a MongoDB collection
+           - it specifies the fields and their types, default values, validators, etc.
+*/          
 
 const noteSchema = new mongoose.Schema({
     content: {
@@ -7,13 +14,21 @@ const noteSchema = new mongoose.Schema({
         required: true
     },
     important: Boolean,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User' // indicate that this field has relationship to the User model
+    }
 })
+
+
+// Define the toJSON method for the schema
+// This method is used to format the object returned by Mongoose
 
 noteSchema.set('toJSON', {
     transform:(document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString() // change _id to id
+        delete returnedObject._id // remove _id
+        delete returnedObject.__v // remove __v
     }
 })
 
