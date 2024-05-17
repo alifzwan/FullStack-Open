61eb81ab1d9a2c174232b80c658584546a1642a6
alifzwan <alifzakwan529@gmail.com>
2024-05-17 12:02:47 +0800

    chore: Generate JSON Web Token (JWT) for user authentication

--- part4/part4-notes-backend/controllers/login.js
+++ part4/part4-notes-backend/controllers/login.js
@@ -21,10 +21,22 @@ loginRouter.post('/', async (request, response) => {
 
     // if the user is found and the password is correct,
     // create a token for the user
+
+    // PURPOSE OF TOKEN
+    // - to authenticate the user for subsequent requests after they logged in
+    // - Once user is authenticated, the server sends this token to the client
+    // - The client then includes this token in the header of subsequent requests
+    // - The server can then verify the token and authenticate user's request without needing
+    //   to ask for username and password again
+
+    
     const userForToken = {
         username: user.username,
         id: user._id
     }
+
+    // token is generated using JSON Web Token (JWT) 
+    // which signed with the payload (userForToken) and secret key (process.env.SECRET)
 
     const token = jwt.sign(userForToken, process.env.SECRET) // jwt - sign the token with the userForToken and SECRET
 
