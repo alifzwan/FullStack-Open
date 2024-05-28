ac80857b6fd5686c39f6f02effb2155bf4fb9d18
alifzwan <alifzakwan529@gmail.com>
2024-05-28 15:54:18 +0800

    chore: Refactor users.js controller for user management

--- part4/BlogList/controllers/users.js
+++ part4/BlogList/controllers/users.js
@@ -1,26 +1,31 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
-
-// Bcrypt - its a library for hashing passwords
-// Saltround - determine the complexity of the hash. Higher number, higher complexity
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
 
 usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body;
 
-    const { username, name, password } = request.body // The request body is expected to contain a username, name, and password.
-
-    const saltRounds = 10 // the number of salt rounds is set to 10.
-    const passwordHash = await bcrypt.hash(password, saltRounds) // The password is hashed with the bcrypt library
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
     const user = new User({
-        username,
-        name,
-        passwordHash
+        username, 
+        name, 
+        passwordHash,
     })
 
     const savedUser = await user.save()
-
     response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.delete('/:id', async (request, response) => {
+    await User.findByIdAndDelete(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = usersRouter
